a9726c8f1d4100fdaffda1e3467fda83afee32fb
sabhi128 <sabhiahm12@gmail.com>
2026-01-02 23:14:25 +0500

    Optimize cursor responsiveness

--- erp/src/App.jsx
+++ erp/src/App.jsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import NetworkDebugger from './components/NetworkDebugger' // Debugger
+import CustomCursor from './components/shared/CustomCursor'
 
 function App() {
   return (
     <>
+      <CustomCursor />
       <RouterProvider router={router} />
       <NetworkDebugger />
     </>

3860519cdd88b7109a960c74d82380e4de0d31b9
sabhi128 <sabhiahm12@gmail.com>
2026-01-08 19:44:27 +0500

    Remove Custom Cursor for native mouse behavior

--- erp/src/App.jsx
+++ erp/src/App.jsx
@@ -1,12 +1,10 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import NetworkDebugger from './components/NetworkDebugger' // Debugger
-import CustomCursor from './components/shared/CustomCursor'
 
 function App() {
   return (
     <>
-      <CustomCursor />
       <RouterProvider router={router} />
       <NetworkDebugger />
     </>

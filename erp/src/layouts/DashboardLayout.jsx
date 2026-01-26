a591110ddcb37b043b422f25d9f499e6f4f1f185
sabhi128 <sabhiahm12@gmail.com>
2026-01-26 14:56:30 +0500

    layout issue in finance:

--- erp/src/layouts/DashboardLayout.jsx
+++ erp/src/layouts/DashboardLayout.jsx
@@ -357,18 +357,9 @@ export default function DashboardLayout() {
 
                 {/* Page Content */}
                 <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-auto z-10 relative">
-                    <AnimatePresence mode="wait">
-                        <motion.div
-                            key={location.pathname}
-                            initial={{ opacity: 0, y: 20 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            exit={{ opacity: 0, y: -20 }}
-                            transition={{ duration: 0.2 }}
-                            className="w-full max-w-7xl mx-auto"
-                        >
-                            {currentOutlet}
-                        </motion.div>
-                    </AnimatePresence>
+                    <div className="w-full max-w-7xl mx-auto">
+                        {currentOutlet}
+                    </div>
                 </main>
             </div>
         </div>
